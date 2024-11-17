0a47e2fcd1dbe4de3a645f5e5a49f12eb8e7e58b
MatiasBH <mbenavides@utem.cl>
2024-11-16 21:02:37 -0300

    add transitions

---
+++ GDTrabajo de titulo/Scenes/Entradas/entrada.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var transition = $ParallaxBackground/ParallaxLayer/Entrada/Transition
+
+var next_scene = "" 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	transition.play("fade_in")
+
+# Función para el primer botón
+func _on_button_pressed() -> void:
+	next_scene = "res://Scenes/Patios/Patio_1.tscn"
+	transition.play("fade_out")
+
+# Función para el segundo botón
+func _on_button_2_pressed() -> void:
+	next_scene = "res://Scenes/Entradas/Escaleras_Lab.tscn"
+	transition.play("fade_out")
+
+# Función para el cambio de escena
+func _on_transition_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file(next_scene)
---
+++ GDTrabajo de titulo/Scenes/Patios/patio_1.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+@onready var transition = $ParallaxBackground/ParallaxLayer/Patio_1/Transition
+
+var next_scene = "" 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	transition.play("fade_in")
+
+# Función para el primer botón
+func _on_button_pressed() -> void:
+	next_scene = "res://Scenes/Entradas/entrada.tscn"
+	transition.play("fade_out")
+
+# Función para el segundo botón
+func _on_button_2_pressed() -> void:
+	next_scene = "res://Scenes/M3/Pasillo_m3.tscn"
+	transition.play("fade_out")
+
+# Función para el tercer botón
+func _on_button_3_pressed() -> void:
+	next_scene = "res://Scenes/Patios/Patio_casino.tscn"
+	transition.play("fade_out")
+
+# Función para el cuarto botón
+func _on_button_4_pressed() -> void:
+	next_scene = "res://Scenes/Extras/Diario_mural.tscn"
+	transition.play("fade_out")
+
+# Función para el cambio de escena
+func _on_transition_animation_finished(anim_name: StringName) -> void:
+	get_tree().change_scene_to_file(next_scene)
+
+
+
+	
