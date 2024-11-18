27c9fbc45ca24ce200fe7bc18155b3b5901877fb
MatiasBH <mbenavides@utem.cl>
2024-11-18 19:23:17 -0300

    add sfx

--- GDTrabajo de titulo/Scripts/escaleras_lab.gd
+++ GDTrabajo de titulo/Scripts/escaleras_lab.gd
@@ -11,10 +11,12 @@ func _ready() -> void:
 func _on_button_pressed() -> void:
 	next_scene = "res://Scenes/Labs/Entrada_Labs.tscn"
 	transition.play("fade_out")
+	AudioManager.steps_audio.play()
 
 func _on_button_2_pressed() -> void:
 	next_scene = "res://Scenes/Entradas/entrada.tscn"
 	transition.play("fade_out")
+	AudioManager.steps_audio.play()
 
 func _on_transition_animation_finished(anim_name: StringName) -> void:
 	get_tree().change_scene_to_file(next_scene)
